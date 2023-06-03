c7889c2e0c91563a6d01ae4032a7943721715c39
Muhaimin Habib <habib_scci@hotmail.com>
2023-06-03 23:59:45 +0600

    Remove Unused Files

--- src/views/dashboards/analytics/dmp/AnalyticsCongratulations.tsx
+++ src/views/dashboards/analytics/dmp/AnalyticsCongratulations.tsx
@@ -32,7 +32,7 @@ const AnalyticsCongratulations = () => {
   // ** Hook
 
   // const theme = useTheme()
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext)
 
   return (
     <Card sx={{ position: 'relative' }}>
@@ -43,16 +43,7 @@ const AnalyticsCongratulations = () => {
               Welcome {user?.username}! 🎉
             </Typography>
             <Typography sx={{ color: 'text.secondary' }}>Get ready to set and check your digital assets.</Typography>
-
-            {/* <Typography sx={{ mb: 3, color: 'text.secondary' }}>Check your new badge in your profile.</Typography>
-            <Button size='small' variant='outlined'>
-              View Badges
-            </Button> */}
           </Grid>
-
-          {/* <StyledGrid item xs={12} sm={6}>
-            <Img alt='Congratulations John' src={`/images/cards/illustration-john-${theme.palette.mode}.png`} />
-          </StyledGrid> */}
         </Grid>
       </CardContent>
     </Card>
