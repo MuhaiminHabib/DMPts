add710c0c8b790a6caa71ee849f60e71837f3ed9
Muhaimin Habib <habib_scci@hotmail.com>
2023-07-18 02:21:25 +0600

    Create Schedule Page

---
+++ src/pages/apps/post/scheduled/index.tsx
@@ -0,0 +1,153 @@
+// ** React Imports
+import { useContext, useState } from 'react'
+
+// ** MUI Imports
+import { Box, Card, CardHeader, Grid, TextField, Typography } from '@mui/material'
+import AddPostDrawer from 'src/views/apps/post/list/AddPostDrawer'
+import {
+  useDeletePostMutation,
+  useFetchPostsQuery,
+  useFetchPostsforCQuery,
+  useFetchPostsforCmQuery,
+  useFetchPostsforDMQuery
+} from 'src/store/query/postApi'
+import { showErrorAlert, showLoadingAlert, showSuccessAlert } from 'src/utils/swal'
+import Swal from 'sweetalert2'
+import PostListTable from 'src/views/apps/post/list/PostListTable'
+import TableHeader from 'src/views/apps/post/list/TableHeader'
+import { AbilityContext } from 'src/layouts/components/acl/Can'
+import { AuthContext } from 'src/context/AuthContext'
+
+const ScheduledList = () => {
+  // ** State
+
+  // const [postPage, setPostPage] = useState<string>('1')
+
+  const postPage = '1'
+  const [addPostOpen, setAddPostOpen] = useState<boolean>(false)
+
+  // ** Hooks
+  const { user } = useContext(AuthContext)
+  const ability = useContext(AbilityContext)
+  const {
+    isFetching,
+
+    // isError, error,
+    data: posts
+  } = useFetchPostsQuery(postPage)
+
+  const {
+    isFetching: isFetchingFetchPostsforDm,
+
+    //  isError: isErrorFetchPostsforDm,
+    //  error: FetchPostsforDmError,
+    data: FetchPostsforDmData
+  } = useFetchPostsforDMQuery()
+
+  const {
+    isFetching: isFetchingFetchPostsforC,
+
+    //  isError: isErrorFetchPostsforC,
+    //  error: FetchPostsforCError,
+    data: FetchPostsforCData
+  } = useFetchPostsforCQuery()
+  const {
+    isFetching: isFetchingFetchPostsforCm,
+
+    //  isError: isErrorFetchPostsforCm,
+    //  error: FetchPostsforCmError,
+    data: FetchPostsforCmData
+  } = useFetchPostsforCmQuery()
+
+  const [
+    deletePost,
+    { isLoading: isLoadingDeletePost, isError: isDeletePostError, error: deletePostError, data: deletePostData }
+  ] = useDeletePostMutation()
+
+  // ** Functions
+
+  const toggleAddPostDrawer = () => setAddPostOpen(!addPostOpen)
+
+  const showDeleteConfirmationPopup = (postId: string, title: string) => {
+    Swal.fire({
+      title: `Do you want to delete ${title}?`,
+      showCancelButton: true,
+      confirmButtonText: 'Proceed',
+      denyButtonText: `Cancel`
+    }).then(result => {
+      if (result.isConfirmed) {
+        deletePost(postId)
+      }
+    })
+  }
+
+  const handlePostDelete = (postId: string, title: string) => {
+    console.log('postId', postId)
+    showDeleteConfirmationPopup(postId, title)
+  }
+
+  if (isLoadingDeletePost) {
+    showLoadingAlert()
+  } else if (isDeletePostError) {
+    showErrorAlert({ error: deletePostError })
+  } else if (deletePostData) {
+    showSuccessAlert({ text: 'Post Deleted' })
+  }
+
+  if (FetchPostsforCData) {
+    console.log('FetchPostsforCData is:', FetchPostsforCData)
+    console.log('posts is:', posts)
+    console.log('FetchPostsforDmData is:', FetchPostsforDmData)
+  }
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Scheduled Posts' />
+          <Box
+            // bgcolor={'red'}
+            justifyItems={'center'}
+            alignItems={'center'}
+            sx={{
+              px: 4
+            }}
+          >
+            <TextField
+              size={'medium'}
+              id='outlined-basic'
+              label='Search by post title or client name'
+              variant='outlined'
+              sx={{ width: 400 }}
+            />
+          </Box>
+
+          <PostListTable
+            isFetching={
+              ((user!.role === 'A' || user!.role === 'BA') && isFetching) ||
+              (user!.role === 'DM' && isFetchingFetchPostsforDm) ||
+              (user!.role === 'C' && isFetchingFetchPostsforC) ||
+              (user!.role === 'CM' && isFetchingFetchPostsforCm)
+            }
+            posts={
+              posts && posts?.length > 0
+                ? posts
+                : FetchPostsforDmData && FetchPostsforDmData.length > 0
+                ? FetchPostsforDmData
+                : FetchPostsforCData && FetchPostsforCData?.length > 0
+                ? FetchPostsforCData
+                : FetchPostsforCmData && FetchPostsforCmData?.length > 0
+                ? FetchPostsforCmData
+                : []
+            }
+            handlePostDelete={handlePostDelete}
+          />
+        </Card>
+      </Grid>
+
+      <AddPostDrawer open={addPostOpen} toggle={toggleAddPostDrawer} />
+    </Grid>
+  )
+}
+
+export default ScheduledList
