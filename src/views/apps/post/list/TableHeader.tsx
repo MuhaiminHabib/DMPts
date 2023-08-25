0d2a5d0fd67df0eadb8d3b91808fbf478ecfa69e
Muhaimin Habib <habib_scci@hotmail.com>
2023-08-26 02:43:33 +0600

    Update File

--- src/views/apps/post/list/TableHeader.tsx
+++ src/views/apps/post/list/TableHeader.tsx
@@ -1,50 +1,58 @@
 // ** MUI Imports
 import { TextField } from '@mui/material'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+import { useRouter } from 'next/router'
+import FilterModal from './FilterModal'
 
 // ** Icon Imports
 // import Icon from 'src/@core/components/icon'
 
-// interface TableHeaderProps {
-//   value?: string
-//   toggle: () => void
-//   handleFilter?: (val: string) => void
-// }
+interface TableHeaderProps {
+  setPostList: any
+  setNowShowing: any
+  setPage: any
+  searchPosts: any
+}
 
-const TableHeader = () => {
-  // props: TableHeaderProps
+const TableHeader = ({ setPostList, setNowShowing, setPage, searchPosts }: TableHeaderProps) => {
   // ** Props
   // const { toggle } = props
 
   //States
 
   //Hooks
+  const router = useRouter()
 
   //Functions
+  const handleSearchTextChange = (criteria: string) => {
+    searchPosts({
+      criteria,
+      type: router.pathname.split('/')[3].charAt(0).toUpperCase()
+    })
+  }
 
   return (
     <Box
       justifyItems={'center'}
-      alignItems={'center'}
+      alignItems={'center'} // Add this line to vertically center-align the items
       sx={{
         px: 4,
         display: 'flex',
+        flexDirection: 'row',
         gap: 4,
         justifyContent: 'space-between'
       }}
     >
       <TextField
-        size={'medium'}
+        size={'small'}
         id='outlined-basic'
-        label='Search by post title or client name'
+        label='Search by post body or client name'
         variant='outlined'
-        sx={{ width: 400 }}
+        sx={{ width: '100%' }}
+        onChange={e => handleSearchTextChange(e.target.value)}
       />
 
-      <Button variant='outlined' sx={{ width: 200 }} size={'large'}>
-        Filter
-      </Button>
+      <FilterModal setPostList={setPostList} setNowShowing={setNowShowing} setPage={setPage} />
     </Box>
   )
 }
