5df5e6ba843bc3c725792252dc147770a32a474b
Muhaimin Habib <habib_scci@hotmail.com>
2023-08-29 01:46:37 +0600

    Add SimpleLoader

--- src/shared-components/Loader.tsx
+++ src/shared-components/Loader.tsx
@@ -1,12 +1,18 @@
 import { Box, CircularProgress } from '@mui/material'
 import React from 'react'
 
-const Loader = () => {
+export const Loader = () => {
   return (
     <Box display='flex' justifyContent='center' p={'20px'}>
-       <CircularProgress disableShrink />
-     </Box>
+      <CircularProgress disableShrink />
+    </Box>
   )
 }
 
-export default Loader
+export const SimpleLoader = () => {
+  return (
+    <Box>
+      <CircularProgress disableShrink size={20} />
+    </Box>
+  )
+}
